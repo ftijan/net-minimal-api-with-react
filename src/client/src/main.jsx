2620048543f95b7580fa3e3d6ec23b6f14bf6b59
Filip Tijan <ftijan@gmail.com>
2023-10-06 03:26:31 +0100

    add design system and components

--- src/client/src/main.jsx
+++ src/client/src/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+const theme = createTheme();
+
 import Pizza from './Pizza.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Pizza />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Pizza className="Pizza"/>
+    </ThemeProvider>    
   </React.StrictMode>,
 )
