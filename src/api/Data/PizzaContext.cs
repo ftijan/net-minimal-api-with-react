071f2ecdbb4d98e752274b503dcfc7733fb20fd0
Filip Tijan <ftijan@gmail.com>
2023-10-06 02:47:43 +0100

    add c# api

---
+++ src/api/Data/PizzaContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Example.Api.Data
+{
+	public class PizzaContext : DbContext
+	{
+        public PizzaContext(DbContextOptions<PizzaContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Pizza> Pizzas { get; set; }
+    }
+}
